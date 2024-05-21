e8cda5666e17fb4c983192c24118f00632d309aa
MagnoLevi <magnolevi2003@gmail.com>
2024-05-21 18:01:46 -0300

    Commit

---
+++ assets/js/cards.js
@@ -0,0 +1,23 @@
+$("#card_funcionalidade").on("click", function () {
+    $("#modal_funcionalidade").modal("show");
+})
+
+$("#card_confiabilidade").on("click", function () {
+    $("#modal_confiabilidade").modal("show");
+})
+
+$("#card_usabilidade").on("click", function () {
+    $("#modal_usabilidade").modal("show");
+})
+
+$("#card_eficiencia").on("click", function () {
+    $("#modal_eficiencia").modal("show");
+})
+
+$("#card_manutenibilidade").on("click", function () {
+    $("#modal_manutenibilidade").modal("show");
+})
+
+$("#card_portabilidade").on("click", function () {
+    $("#modal_portabilidade").modal("show");
+})
